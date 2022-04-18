38dda29973a4ce9eabf1b6ed06c954c7903d8fe2
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-04-18 22:55:36 +0200

    added type in return ApiConnect

--- src/common/apiConnect.ts
+++ src/common/apiConnect.ts
@@ -1,8 +1,9 @@
 export const apiConect = async (path: string) => {
-    const response = await fetch(`https://fakestoreapi.com/${path}`);
+    const response = await fetch(`https://fakestoreapi.com/products/${path}`);
     console.log(response);
     if (!response.ok) {
         throw new Error(response.statusText);
     }
-    return await response.json();
+    const num: string[] = await response.json();
+    return num
 };
