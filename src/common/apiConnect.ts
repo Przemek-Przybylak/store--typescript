26e3ff227448113f72d0756e1ee729487f1d7773
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-04-20 18:56:57 +0200

    remove useless content from ApiConnect

--- src/common/apiConnect.ts
+++ src/common/apiConnect.ts
@@ -1,9 +1,7 @@
 export const apiConect = async (path: string) => {
     const response = await fetch(`https://fakestoreapi.com/products/${path}`);
-    console.log(response);
     if (!response.ok) {
         throw new Error(response.statusText);
     }
-    const num: string[] = await response.json();
-    return num
+    return await response.json();
 };
