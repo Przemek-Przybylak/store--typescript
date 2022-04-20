7afa8a0d492ba5d324a98c682293fba6e5d134f8
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-04-20 19:00:16 +0200

    added middleware in store

--- src/core/store.ts
+++ src/core/store.ts
@@ -1,11 +1,19 @@
 import {configureStore} from "@reduxjs/toolkit";
+import createSagaMiddleware from "@redux-saga/core";
 import listReducer from '../featores/productsList/productsSlice';
+import {useDispatch} from "react-redux";
+import rootSaga from "./rootSaga";
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
     reducer: {
         list: listReducer,
     },
+    middleware: [sagaMiddleware],
 })
 
+sagaMiddleware.run(rootSaga);
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
+export const useAppDispatch = () => useDispatch<AppDispatch>()
