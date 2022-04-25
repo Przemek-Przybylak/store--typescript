c883c392287c21451ee9431f0a6756b8d20b9874
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-04-25 23:34:24 +0200

    fetchAllProducts async function added to fetch products from API

---
+++ src/featores/productsList/fetchAllProducts..ts
@@ -0,0 +1,14 @@
+import ApiConnect from "../../common/apiConnect";
+import {ProductsListResponse} from "./models/ProductsListResponse";
+
+export async function fetchAllProducts() {
+    try {
+        const data1 = await ApiConnect.get<ProductsListResponse[]>(
+            `products/`,
+        );
+        return data1.data;
+    } catch (error) {
+        const errorText: string = "Oops something went wrong...";
+        return errorText;
+    }
+}
