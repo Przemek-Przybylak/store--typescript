df47948769f645e65c498b23ee9538f6b8d37ed1
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-04-28 20:28:04 +0200

    create TopBar component

---
+++ src/featores/topBar/TopBar/index.js.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Menu} from "../Menu";
+import {Search} from "../search/Search";
+
+export const TopBar: React.FC = () => {
+    return(
+        <>
+            <Menu />
+            <Search />
+        TopBar
+        </>
+    )
+}
