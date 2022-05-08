84754c9c1c7e83861efa1c90a8822e25d96941dc
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-05-08 20:50:20 +0200

    create fetchCategories function

---
+++ src/featores/topBar/TopBar/fetchCategories.ts
@@ -0,0 +1,15 @@
+import ApiConnect from "../../../common/apiConnect";
+import {CategoriesResponse} from "../models/categoriesResponse";
+
+
+export async function fetchCategories() {
+    try {
+        const data1 = await ApiConnect.get<CategoriesResponse[]>(
+            `products/categories`,
+        );
+        return data1.data;
+    } catch (error) {
+        const errorText: string = "Oops something went wrong...";
+        return errorText;
+    }
+}
