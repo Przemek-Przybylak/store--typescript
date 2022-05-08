7d11002a10d65fddf3ae20d5d71e72a868277f47
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-05-08 20:49:48 +0200

    dispatch action fetch categories in TopBar added

--- src/featores/topBar/TopBar/index.js.tsx
+++ src/featores/topBar/TopBar/index.js.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Menu} from "../Menu";
 import {Search} from "../search/Search";
 import {Wrapper} from "./styled";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchList, selectList} from "../../topBar/TopBar/TopBarSlice";
 
 export const TopBar: React.FC = () => {
+    const dispatch = useDispatch();
+    const categories = useSelector(selectList);
+
+    useEffect(() => {
+        dispatch(fetchList());
+    }, [dispatch]);
+
     return(
         <Wrapper>
-            <div>
-                <Menu />
-            </div>
+            { categories &&
+                <div>
+                    <Menu categories={categories}/>
+                </div>}
             <div>
                 <Search />
             </div>
