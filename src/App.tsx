13dcddfdf404fb3bf33552284df9754253c60745
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-05-08 20:53:46 +0200

    added TopBar component in App

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {TopBar} from "./featores/topBar/TopBar/index.js";
 function App() {
     return (
         <div className="App">
+            <TopBar />
             <div>
                 <ProductsList/>
                 nanana
