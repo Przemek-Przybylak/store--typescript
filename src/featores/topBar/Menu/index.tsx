14f3ae48b8ca3b065ed8668b6aff93a89aa4b701
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-05-09 21:01:31 +0200

    added wraper in Menu component

--- src/featores/topBar/Menu/index.tsx
+++ src/featores/topBar/Menu/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {CategoriesResponse} from "../models/categoriesResponse";
+import {Wrapper} from "./styled";
 
 interface props {
     categories: CategoriesResponse[],
@@ -8,7 +9,7 @@ interface props {
 export const Menu: React.FC<props> = ({categories}) => {
   return(
       <>
-          <div>{categories}</div>
+          <Wrapper>{categories}</Wrapper>
       </>
   )
 }
