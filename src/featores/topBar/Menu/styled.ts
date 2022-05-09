621164d1858c38aca0a57e51d02e77f6051bd80a
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-05-09 21:01:06 +0200

    added styles to menu Wapper

---
+++ src/featores/topBar/Menu/styled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.ul`
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: 25px;
+  letter-spacing: 1.5px;
+`;
