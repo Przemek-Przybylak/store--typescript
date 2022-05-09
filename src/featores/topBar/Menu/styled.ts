59430ab74413d3cf573d68f6dec59852e14ac466
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-05-09 21:14:40 +0200

    added styles to Menu Item

--- src/featores/topBar/Menu/styled.ts
+++ src/featores/topBar/Menu/styled.ts
@@ -4,9 +4,16 @@ export const Wrapper = styled.ul`
   padding: 0;
   margin: 0;
   list-style: none;
+  display: flex;
+  flex-direction: row;
+
+`;
+
+export const Item = styled.li`
+  padding: 0px 10px;
   text-decoration: none;
   text-transform: uppercase;
-  font-weight: 700;
+  font-weight: 500;
   font-size: 25px;
   letter-spacing: 1.5px;
 `;
