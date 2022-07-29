19a7f94b386a89734293f1d7b507d0c6e1b73102
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-07-29 19:33:30 +0200

    Alternative box model added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { GlobalStyles } from './featores/GlobalStyles';
 import {ProductsList} from "./featores/productsList/ProductsList";
 import {TopBar} from "./featores/topBar/TopBar/index.js";
 
 function App() {
     return (
         <div className="App">
+            <GlobalStyles />
             <TopBar />
             <div>
                 <ProductsList/>
-                waiting...
             </div>
         </div>
     );
---
+++ src/featores/GlobalStyles.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+      html{
+        box-sizing: border-box;
+    };
+    *, ::after, ::before {
+        box-sizing: inherit;
+    };
+
+    body {
+      transition: 300ms;
+    }
+`
