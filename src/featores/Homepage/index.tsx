36f0fc1db636453a0d9261244671117d040c9e01
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-07-29 20:57:31 +0200

    created Homepage component

---
+++ src/featores/Homepage/index.tsx
@@ -0,0 +1,12 @@
+import { ProductsList } from "../productsList/ProductsList/";
+import { TopBar } from "../topBar/TopBar/index.js";
+
+export const Homepage: React.FC = () => {
+
+return (
+  <>
+  <TopBar />
+  <ProductsList/>
+  </>
+)
+}
