0bc33b8f944b637324ef21489a000fe1e5077440
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-07-31 19:49:41 +0200

    added styled Link in Menu

--- src/common/topBar/Menu/index.tsx
+++ src/common/topBar/Menu/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { StyledLink } from "../../StyledLink";
 import { CategoriesResponse } from "../models/categoriesResponse";
 import { Wrapper, Item } from "./styled";
 
@@ -11,7 +12,11 @@ export const Menu: React.FC<props> = ({ categoriesList }) => {
     <>
       <Wrapper>
         {categoriesList &&
-          categoriesList.map((category) => <Item>{category}</Item>)}
+          categoriesList.map((category) => (
+            <Item>
+              <StyledLink to={`/`}>{category}</StyledLink>
+            </Item>
+          ))}
       </Wrapper>
     </>
   );
