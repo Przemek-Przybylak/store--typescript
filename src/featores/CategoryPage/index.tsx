0218d3f0ddf7a8276e47e68237bdde970faff934
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-07-31 11:09:30 +0200

    move TopBar into common direction

---
+++ src/featores/CategoryPage/index.tsx
@@ -0,0 +1,12 @@
+import { ProductsList } from "../productsList/ProductsList/";
+import { TopBar } from "../topBar/TopBar/index.js";
+
+export const CategoriesPage: React.FC = () => {
+
+return (
+  <>
+  <TopBar />
+  <ProductsList/>
+  </>
+)
+}
