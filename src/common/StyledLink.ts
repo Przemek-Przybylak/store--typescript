535ab3ab467afbf0c54ab83db883a11571fcae97
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-07-31 19:49:10 +0200

    StyledLink added

---
+++ src/common/StyledLink.ts
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const StyledLink = styled(Link)`
+    text-decoration: none;
+    color: inherit;
+    background-color: inherit;
+`;
