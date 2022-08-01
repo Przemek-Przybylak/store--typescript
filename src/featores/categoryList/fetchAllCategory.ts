3dc43d92bbbb34960082ed6324ae0244da4b88f6
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-08-01 17:28:02 +0200

    fetch category function added

---
+++ src/featores/categoryList/fetchAllCategory.ts
@@ -0,0 +1,15 @@
+import apiConnect from "../../common/apiConnect";
+import { ProductsListResponse } from "../../common/models/ProductsListResponse";
+
+export async function fetchAllCategory() {
+  try {
+        const query: string = window.location.hash.substr(2);
+        const data1 = await apiConnect.get<ProductsListResponse[]>(
+          `products/category/${query}`,
+      );
+      return data1.data;
+  } catch (error) {
+      const errorText: string = "Oops something went wrong...";
+      return errorText;
+  }
+}
