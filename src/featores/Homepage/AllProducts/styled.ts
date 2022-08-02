7e040ac74c5984f54e350189d3a7aed7d457d612
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-08-02 17:45:36 +0200

    remove wrapper from AllProducts styles

--- src/featores/Homepage/AllProducts/styled.ts
+++ src/featores/Homepage/AllProducts/styled.ts
@@ -1,14 +1,5 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 20px;
-  list-style: none;
-  padding: 0;
-  overflow-y: hidden;
-`;
-
 export const Product = styled.li`
   width: 100%;
   display: flex;
