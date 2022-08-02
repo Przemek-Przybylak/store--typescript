5a44df600739dbb598cb26f52194054be581604c
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-08-02 16:22:55 +0200

    removed dubbuging console.log in fetchAllCategory

--- src/featores/categoryList/fetchAllCategory.ts
+++ src/featores/categoryList/fetchAllCategory.ts
@@ -2,9 +2,8 @@ import apiConnect from "../../common/apiConnect";
 import { ProductsListResponse } from "../../common/models/ProductsListResponse";
 
 export async function fetchAllCategory() {
-  try {console.log("działa")
+  try {
         const query: string = window.location.hash.substr(11);
-        console.log(query)
         const data1 = await apiConnect.get<ProductsListResponse[]>(
           `products/category/${query}`,
       );
