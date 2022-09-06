3b1fe1017e167cd67472543c0a6b69137fbc131e
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-09-06 17:13:14 +0200

    fetchProducts function added

---
+++ src/featores/productPage/fetchProduct.ts
@@ -0,0 +1,15 @@
+import apiConnect from "../../common/apiConnect";
+import { ProductsListResponse } from "../../common/models/ProductsListResponse";
+
+export async function fetchProduct() {
+  try {
+        const query: string = window.location.hash.substr(10);
+        const data1 = await apiConnect.get<ProductsListResponse>(
+          `products/${query}`,
+      );
+      return data1.data;
+  } catch (error) {
+      const errorText: string = "Oops something went wrong...";
+      return errorText;
+  }
+}
