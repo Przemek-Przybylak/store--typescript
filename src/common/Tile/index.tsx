1c94d067906c56b8ff62e0f7b4fa291af90418e4
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-09-10 14:58:52 +0200

    transferred product into StyledLink

--- src/common/Tile/index.tsx
+++ src/common/Tile/index.tsx
@@ -9,36 +9,38 @@ interface props {
 export const Tile: React.FC<props> = ({ products, categoryPage }) => {
   return (
     <>
-      {products instanceof Array && categoryPage ? (
-        products.map(({ id, image, price, title }) => (
-          <ItemWrapper>
-            <Product key={id}>
+      <Wrapper>
+        {products instanceof Array && categoryPage ? (
+          products.map(({ id, image, price, title }) => (
+            <ItemWrapper>
               <StyledLink to={`/product:${id}`}>
+                <Product key={id}>
+                  <Image src={image} />
+                  <span>{title}</span>
+                  <span>{price} $</span>
+                </Product>
+              </StyledLink>
+            </ItemWrapper>
+          ))
+        ) : products instanceof Array ? (
+          products.map(({ id, image, category }) => (
+            <Product key={id}>
+              <StyledLink to={`/category:${category}`}>
                 <Image src={image} />
-                <span>{title}</span>
-                <span>{price} $</span>
               </StyledLink>
             </Product>
-          </ItemWrapper>
-        ))
-      ) : products instanceof Array ? (
-        products.map(({ id, image, category }) => (
-          <Product key={id}>
-            <StyledLink to={`/category:${category}`}>
-              <Image src={image} />
-            </StyledLink>
+          ))
+        ) : products instanceof Object ? (
+          <Product key={products.id}>
+            <Image src={products.image} />
+            <span>{products.title}</span>
+            <span>{products.id}</span>
+            <span>{products.description}</span>
           </Product>
-        ))
-      ) : products instanceof Object ? (
-        <Product key={products.id}>
-          <Image src={products.image} />
-          <span>{products.title}</span>
-          <span>{products.id}</span>
-          <span>{products.description}</span>
-        </Product>
-      ) : (
-        ""
-      )}
+        ) : (
+          ""
+        )}
+      </Wrapper>
     </>
   );
 };
