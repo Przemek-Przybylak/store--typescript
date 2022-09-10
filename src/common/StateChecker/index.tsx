6db47a75727d77a287f8581fe5acb91334d325e1
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-09-10 16:39:52 +0200

    State checker component added

---
+++ src/common/StateChecker/index.tsx
@@ -0,0 +1,23 @@
+import { Status } from "../models/status";
+import { StateContent, Wrapper } from "./styled";
+interface props {
+  children: React.FC;
+  status: Status;
+}
+export const StateChecker: React.FC<props> = ({ children, status }) => {
+  return (
+    <>
+      {status === "loading" ? (
+        <Wrapper>
+          <StateContent>Loading..</StateContent>
+        </Wrapper>
+      ) : status === "error" ? (
+        <Wrapper>
+          <StateContent>Oooopss... something went wrong</StateContent>
+        </Wrapper>
+      ) : (
+        children
+      )}
+    </>
+  );
+};
---
+++ src/common/StateChecker/styled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+  height: 50vh;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50vw;
+`;
+export const StateContent = styled.p`
+  font-size: 50px;
+  text-align: center;
+  font-weight: 700;
+`;
