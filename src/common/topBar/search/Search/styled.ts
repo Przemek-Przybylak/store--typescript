48faec86238a0e0ca811d129598740a85ebb4b19
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-09-11 19:19:37 +0200

    better styles for Search Input added

--- src/common/topBar/search/Search/styled.ts
+++ src/common/topBar/search/Search/styled.ts
@@ -7,8 +7,8 @@ export const Wrapper = styled.div`
 export const Input = styled.input`
   border: none;
   outline: 1px solid grey;
+  padding: 4px 10px;
   border-radius: 20px;
-  font-size: 25px;
-
-
+  font-size: 20px;
+  color: grey;
 `;
