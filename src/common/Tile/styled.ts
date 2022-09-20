47710ff2dfa98e313027507ec56675b2845a7210
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-09-20 19:12:35 +0200

    better styles for Tiles added

--- src/common/Tile/styled.ts
+++ src/common/Tile/styled.ts
@@ -1,22 +1,34 @@
 import styled, { css } from "styled-components";
+import { theme } from "../../core/theme";
 
 
 export const Wrapper = styled.ul`
   display: grid;
+  align-content: center;
   grid-template-columns: repeat(3, 1fr);
-  gap: 20px;
+  gap:20px;
+  margin: 20px auto;
   list-style: none;
   padding: 0;
   overflow-y: hidden;
-  max-width: 100%;
+  max-width: 1000px;
+  align-items:center;
 `;
 
 export const Product = styled.li<{ vertical?: boolean}>`
   width: 100%;
+  padding: 10px;
   display: flex;
   justify-content: center;
   flex-direction: column;
   align-items: center;
+  border: 1px solid ${theme.color.lightGrey};
+  border-radius: 10px;
+  height: 400px;
+
+  :hover{
+    border: 1px solid ${theme.color.eerieBlack};
+  }
 
   ${({vertical}) => vertical && css`
   flex-direction: row;
@@ -36,8 +48,8 @@ export const Span = styled.span`
 `;
 
 export const Image = styled.img`
-  margin-bottom: 20px;
-  max-width: 400px;
+  margin-bottom: 10px;
+  max-width: 250px;
   height: 300px;
   transition: 300ms;
   background-color: inherit;
