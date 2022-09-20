b9e1519cf3670877faf6106bede82c11d8db0e3f
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-09-20 18:02:47 +0200

    added theme

---
+++ src/core/theme.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  color: {
+    eerieBlack:"#1A1A1A",
+    moonstoneBlue:"#B78277",
+    macaroniAndCheese: "#97DFF8",
+    oceanGreen: "#3FC495",
+  },
+  breakpoint: {
+    mobileMax: "767px",
+    tabletMax: "992px",
+  },
+};
