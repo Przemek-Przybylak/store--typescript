1a3f73ca620f86f619b1d2f4af239a1e942b0bb7
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-09-24 19:18:57 +0200

    color for hover state in StyledLink added

--- src/common/StyledLink.ts
+++ src/common/StyledLink.ts
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { theme } from "../core/theme";
 
 export const StyledLink = styled(Link)`
     text-decoration: none;
     color: inherit;
     background-color: inherit;
-    transition: 300ms;
+    transition: 400ms;
 
-    &:hover {
-        color: #888;
-    }
+    :hover{
+    color:${theme.color.moonstoneBlue};
+  }
 `;
