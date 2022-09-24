bb2da934946b86163173c17758e68f64d1b1c648
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-09-24 20:51:03 +0200

    new styles to TopBar Wrapper added

--- src/common/topBar/TopBar/styled.ts
+++ src/common/topBar/TopBar/styled.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { theme } from "../../../core/theme";
 
 export const Wrapper = styled.div`
-  padding: 20px;
+  padding: 20px 20px 0;
   display: flex;
   justify-content: space-between;
   align-items: center;
