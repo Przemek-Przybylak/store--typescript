93ac50d294cf563138a4ff7f47edb95ee94f5535
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-09-24 19:21:23 +0200

    better styles in TopBar component added

--- src/common/topBar/TopBar/styled.ts
+++ src/common/topBar/TopBar/styled.ts
@@ -1,9 +1,14 @@
 import styled from "styled-components";
+import { theme } from "../../../core/theme";
 
 export const Wrapper = styled.div`
   padding: 20px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  border-bottom: 1px solid #ccc;
+  position: sticky;
+  background-color: ${theme.color.white};
+  color: ${theme.color.macaroniAndCheese};
+  top: 0px;
+  z-index: 1;
 `;
