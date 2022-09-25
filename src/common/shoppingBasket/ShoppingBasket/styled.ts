7586d18c324796465a2dad43c51fdacdf8d80896
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-09-25 12:34:04 +0200

    added styles for basket icon

---
+++ src/common/shoppingBasket/ShoppingBasket/styled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { theme } from "../../../core/theme";
+import { ReactComponent as Basket } from "../pictures/basket.svg";
+
+export const Wrapper = styled.div`
+  margin: 0;
+`;
+
+export const BasketIcon = styled(Basket)`
+  height: 31px;
+  fill: ${theme.color.macaroniAndCheese};
+`
