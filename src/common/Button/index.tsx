e0f7a8be1fe1e4a0ba2b6dff921fd25ee10f73a4
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-09-27 21:20:41 +0200

    OnClick function in Button added

--- src/common/Button/index.tsx
+++ src/common/Button/index.tsx
@@ -1,9 +1,21 @@
+import { useDispatch } from "react-redux";
+import { addProduct } from "../commonSlice";
+import { ProductsListResponse } from "../models/ProductsListResponse";
 import { Wrapper } from "./styled";
+interface props {
+  product: ProductsListResponse | ProductsListResponse[];
+}
 
-export const Button: React.FC = () => {
+export const Button: React.FC<props> = ({ product }) => {
+  const dispatch = useDispatch();
+
+  const onClick = () => {
+    dispatch(addProduct(product));
+  };
+
   return (
     <>
-      <Wrapper>buy</Wrapper>
+      <Wrapper onClick={onClick}>buy</Wrapper>
     </>
   );
 };
