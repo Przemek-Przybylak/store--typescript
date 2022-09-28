c0057e34b5919eaa44d77c33f284dc3272addff9
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-09-28 20:07:24 +0200

    added display of products from the basket

--- src/featores/shoppingBasket/ShoppingBasketPage/index.tsx
+++ src/featores/shoppingBasket/ShoppingBasketPage/index.tsx
@@ -1,3 +1,23 @@
+import { useSelector } from "react-redux";
+import { selectList } from "../../../common/commonSlice";
+
 export const ShoppingBasketPage: React.FC = () => {
-  return <>ShoppingBasketPage</>;
+  const products = useSelector(selectList);
+  console.log(products);
+
+  return (
+    <>
+      <div>
+        {products.map(({ id, title, price, image, basketID }) => (
+          <div>
+            <div>{id}</div>
+            <div>{title}</div>
+            <div>{price}</div>
+            <div>{image}</div>
+            <div>{basketID}</div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
