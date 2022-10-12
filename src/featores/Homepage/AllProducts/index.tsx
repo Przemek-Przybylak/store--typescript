ef04cc554103ff556f649f88d30690e901c5ada6
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-10-12 20:05:15 +0200

    ProductsList component in Homepage implement

--- src/featores/Homepage/AllProducts/index.tsx
+++ src/featores/Homepage/AllProducts/index.tsx
@@ -1,10 +1,10 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchList, selectListByQuery, selectStatus } from "./productsSlice";
-import { Tile } from "../../../common/Tile";
 import { StateChecker } from "../../../common/StateChecker";
 import { useQueryParameter } from "../../../common/queryParamters";
 import { RootState } from "../../../core/store";
+import { ProductsList } from "../../../common/ProductsList";
 
 export const AllProductsList: React.FC = () => {
   const dispatch = useDispatch();
@@ -20,11 +20,7 @@ export const AllProductsList: React.FC = () => {
 
   return (
     <StateChecker status={status}>
-      <>
-        {products.map((product) => (
-          <Tile product={product} />
-        ))}
-      </>
+      <ProductsList products={products} />
     </StateChecker>
   );
 };
