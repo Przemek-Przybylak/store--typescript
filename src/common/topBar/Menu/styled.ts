ce4e6c0d25ddf32f309fd7cb49df1256446b64fb
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-10-16 21:34:13 +0200

    CategoriesFlex added for create space menu/search

--- src/common/topBar/Menu/styled.ts
+++ src/common/topBar/Menu/styled.ts
@@ -3,6 +3,7 @@ import { theme } from "../../../core/theme";
 import { ReactComponent as logo } from "./pictures/logo.svg";
 
 export const Wrapper = styled.ul`
+  max-width: 100%;
   padding: 0;
   margin: 0;
   list-style: none;
@@ -10,15 +11,21 @@ export const Wrapper = styled.ul`
   flex-direction: row;
   align-items: center;
 `;
+export const CategoriesFlex = styled.div`
+  display: flex;
+  max-width: 100%;
+  justify-content: space-around;
+  align-items: center;
+`;
 
 export const Item = styled.li`
-  padding: 0px 10px;
   text-decoration: none;
   text-transform: uppercase;
   font-weight: 500;
   font-size: 25px;
   letter-spacing: 1.5px;
   white-space: nowrap;
+  padding: 0 5px;
 `;
 
 export const Logo = styled(logo)`
