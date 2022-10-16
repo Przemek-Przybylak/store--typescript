218ee9704477e9e523be4ad723d68735ee52cf49
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-10-16 21:33:18 +0200

    added Container into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Switch } from "react-router";
 import { Route, HashRouter } from "react-router-dom";
+import { Container } from "./common/Container";
 import { CategoryPage } from "./featores/categoryPage";
 import { Homepage } from "./featores/Homepage";
 import { ProductPage } from "./featores/productPage";
@@ -8,22 +9,24 @@ import { ShoppingBasketPage } from "./featores/shoppingBasket/ShoppingBasketPage
 function App() {
   return (
     <div className="App">
-      <HashRouter>
-        <Switch>
-          <Route path="/category:category">
-            <CategoryPage />
-          </Route>
-          <Route path="/product:id">
-            <ProductPage />
-          </Route>
-          <Route path="/basket">
-            <ShoppingBasketPage />
-          </Route>
-          <Route path="/">
-            <Homepage />
-          </Route>
-        </Switch>
-      </HashRouter>
+      <Container>
+        <HashRouter>
+          <Switch>
+            <Route path="/category:category">
+              <CategoryPage />
+            </Route>
+            <Route path="/product:id">
+              <ProductPage />
+            </Route>
+            <Route path="/basket">
+              <ShoppingBasketPage />
+            </Route>
+            <Route path="/">
+              <Homepage />
+            </Route>
+          </Switch>
+        </HashRouter>
+      </Container>
     </div>
   );
 }
